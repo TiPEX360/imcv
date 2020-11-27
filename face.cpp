fbd1f087f61389dd31351f7083c37d250b423a76
yw18938 <yw18938@bristol.ac.uk>
2020-11-27 18:13:28 +0000

    task1 done

--- face.cpp
+++ face.cpp
@@ -17,12 +17,11 @@
 #include <string>
 
 using namespace std;
-using namespace cv;
 
 
 /** Global variables */
-String cascade_name = "frontalface.xml";
-CascadeClassifier cascade;
+cv::String cascade_name = "frontalface.xml";
+cv::CascadeClassifier cascade;
 
 
 std::vector<std::string> str_split(const std::string &line, char delimiter) {
@@ -38,15 +37,15 @@ std::vector<std::string> str_split(const std::string &line, char delimiter) {
 	return tokens;
 }
 
-vector<Rect> readGroundTruth(string path) {
+vector<cv::Rect> readGroundTruth(string path) {
 	ifstream file(path, ifstream::in);
 	string line;
 	
-	vector<Rect> truths;
+	vector<cv::Rect> truths;
 	getline(file, line);
 	while(!file.eof()) {
 		vector<string> tokens = str_split(line, ' ');
-		truths.push_back(Rect(stoi(tokens[0]), stoi(tokens[1]), stoi(tokens[2]), stoi(tokens[3])));
+		truths.push_back(cv::Rect(stoi(tokens[0]), stoi(tokens[1]), stoi(tokens[2]), stoi(tokens[3])));
 		getline(file, line);
 	}
 	file.close();
@@ -54,46 +53,73 @@ vector<Rect> readGroundTruth(string path) {
 	return truths;
 }
 
-float calcIOU(vector<Rect> faces, vector<Rect> truths) {
+float calcIOU(cv::Rect detected, vector<cv::Rect> truths) {
 	float intersect = 0;
 	float area = 0;
-	for(int face = 0; face < faces.size(); face++) {
-		for(int truth = 0; truth < truths.size(); truth++) {
-			intersect += (faces[face] & truths[truth]).area();
-			area += faces[face].area() + truths[truth].area();
+	float rectUnion = 0;
+	float iou = 0;
+	for(int truth = 0; truth < truths.size(); truth++) {
+		if((detected & truths[truth]).area() > 0) {
+			intersect += (detected & truths[truth]).area();
+			area += detected.area() + truths[truth].area();
 		}
 	}
-	int rUnion = area - intersect;
-	return (intersect / rUnion);
+
+	if(area > 0) {
+		rectUnion = area - intersect;
+		iou = intersect / rectUnion;
+	}
+	return iou;
 }
 
 /** @function detectAndDisplay */
-void detectAndDisplay(Mat frame, string truthsPath)
+void detectAndDisplay(cv::Mat frame, string truthsPath)
 {
-	std::vector<Rect> faces;
-	Mat frame_gray;
+	std::vector<cv::Rect> detected;
+	cv::Mat frame_gray;
+	float threshold = 0.3f;
 
 	// 1. Prepare Image by turning it into Grayscale and normalising lighting
 	cvtColor( frame, frame_gray, CV_BGR2GRAY );
 	equalizeHist( frame_gray, frame_gray );
 
 	// 2. Perform Viola-Jones Object Detection 
-	cascade.detectMultiScale( frame_gray, faces, 1.1, 1, 0|CV_HAAR_SCALE_IMAGE, Size(50, 50), Size(500,500) );
+	cascade.detectMultiScale( frame_gray, detected, 1.1, 1, 0|CV_HAAR_SCALE_IMAGE, cv::Size(50, 50), cv::Size(500,500) );
 
     // 3. Print number of Faces found
-	std::cout << faces.size() << std::endl;
+	std::cout << detected.size() << std::endl;
 
 	//Draw ground truth
-	vector<Rect> truths = readGroundTruth(truthsPath);
+	vector<cv::Rect> truths = readGroundTruth(truthsPath);
 	for(int i = 0; i < truths.size(); i++) {
-		rectangle(frame, Point(truths[i].x, truths[i].y), Point(truths[i].x + truths[i].width, truths[i].y + truths[i].height), Scalar(0, 0, 255), 2);
+		rectangle(frame, cv::Point(truths[i].x, truths[i].y), cv::Point(truths[i].x + truths[i].width, truths[i].y + truths[i].height), cv::Scalar(0, 0, 255), 2);
 	}
-	cout << "IOU = " << calcIOU(faces, truths) << endl;
 
-    // 4. Draw box around faces found
-	for( int i = 0; i < faces.size(); i++ )
+	int positives = truths.size();
+	int noDetected = detected.size();
+	
+	int truePositives = 0;
+	int falsePositives = noDetected - truePositives;
+	int trueNegatives = 0;
+	int falseNegatives = 0;
+	for(int i = 0; i < detected.size(); i++) {
+		float iou = calcIOU(detected[i], truths);
+		if(iou > threshold) truePositives++;
+	}
+	falseNegatives = positives - truePositives;
+
+	cout  << "True Positives: " << truePositives << endl << "False Positives: " << noDetected - truePositives << endl;
+	
+	cout << "False Negatives: " << falseNegatives << endl;
+	float tpr = (float)truePositives / (float)(truePositives + falseNegatives);
+	cout << "TPR = " << tpr << endl;
+	float f1 = (float)truePositives / (float)(truePositives + 0.5f*(float)(falsePositives+falseNegatives));
+	cout << "f1-score = " << f1 << endl;
+
+	// 4. Draw box around faces found
+	for( int i = 0; i < detected.size(); i++ )
 	{
-		rectangle(frame, Point(faces[i].x, faces[i].y), Point(faces[i].x + faces[i].width, faces[i].y + faces[i].height), Scalar( 0, 255, 0 ), 2);
+		rectangle(frame, cv::Point(detected[i].x, detected[i].y), cv::Point(detected[i].x + detected[i].width, detected[i].y + detected[i].height), cv::Scalar( 0, 255, 0 ), 2);
 	}
 
 }
@@ -102,7 +128,7 @@ void detectAndDisplay(Mat frame, string truthsPath)
 int main( int argc, const char** argv )
 {
     // 1. Read Input Image
-	Mat frame = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+	cv::Mat frame = cv::imread(argv[1], CV_LOAD_IMAGE_COLOR);
 
 	// 2. Load the Strong Classifier in a structure called `Cascade'
 	if( !cascade.load( cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
@@ -111,7 +137,7 @@ int main( int argc, const char** argv )
 	detectAndDisplay(frame, argv[2]);
 
 	// 4. Save Result Image
-	imwrite( "detected.jpg", frame );
+	cv::imwrite( "detected.jpg", frame );
 
 	return 0;
 }
